9bf3714f537f9a5f75111f755903ebc662d80d10
Koen <kwgroot@gmail.com>
2021-08-25 12:22:26 +0200

    Numbers properly generated and shown in the form of a ulong. Now just requires some polish to make everything look nice.

--- SyncVR_TechDemo/Assets/Scripts/CanvasManager.cs
+++ SyncVR_TechDemo/Assets/Scripts/CanvasManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ccd8817d2832db36c42728af735d1972c1cff555a6d667a42223064a819f56d
-size 706
+oid sha256:da895b8ff973d45929fd0dd8748a8e02b814bbd378cb0ea0f9c1e576b5008b34
+size 1923
--- SyncVR_TechDemo/Assets/Scripts/MovingNumber.cs
+++ SyncVR_TechDemo/Assets/Scripts/MovingNumber.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e55a046537646145fb44a17e2770e99265d9d8352f0b4aceb315aae340f3a00
-size 1490
+oid sha256:bc231ce94fa521ff82654d1bab24196700b7ea01eb32b0cbf7d04c8ed5938e84
+size 2012
