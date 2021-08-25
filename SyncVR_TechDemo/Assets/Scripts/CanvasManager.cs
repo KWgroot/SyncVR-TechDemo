ad5390e89bdccb70260f6e2874d8074fb9804214
Koen <kwgroot@gmail.com>
2021-08-25 11:47:05 +0200

    Back canvas now generates moving numbers that are upcoming in the fibonacci sequence.

---
+++ SyncVR_TechDemo/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ccd8817d2832db36c42728af735d1972c1cff555a6d667a42223064a819f56d
+size 706
---
+++ SyncVR_TechDemo/Assets/Scripts/MovingNumber.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e55a046537646145fb44a17e2770e99265d9d8352f0b4aceb315aae340f3a00
+size 1490
